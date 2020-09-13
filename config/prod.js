129c5fbf5e11ae01e68bd547f5049375a03c4875
Anita Kerhanaj <akerhana1@veleri.hr>
2020-09-13 15:42:42 +0200

    Heroku

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 // koristi se za produkciju
 module.exports={
-    MongoDB: 'mongodb+srv://carrental:carrental1@carrental.4dktk.mongodb.net/<dbname>?retryWrites=true&w=majority',
-};
+    MongoDB: 'mongodb+srv://carrental:carrental1@carrental.4dktk.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
