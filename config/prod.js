88de7b79bcb8697fa4a6106a30a03564d18e37e1
Anita Kerhanaj <akerhana1@veleri.hr>
2020-09-13 14:48:49 +0200

    hej

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 // koristi se za produkciju
 module.exports={
-    MongoDB:process.env.MongoDB
+    MongoDB: 'mongodb+srv://carrental:carrental1@carrental.4dktk.mongodb.net/<dbname>?retryWrites=true&w=majority',
 };
